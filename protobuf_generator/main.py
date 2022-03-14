57186bfe8b1bb01085f5e480fd492db1fb93c07c
Mattia2700 <mattia2700franzin@gmail.com>
2022-03-14 19:07:39 +0100

    fix(protobuf_generator): remove protobuf compilation

--- protobuf_generator/main.py
+++ protobuf_generator/main.py
@@ -1,22 +1,22 @@
-import subprocess
-import os
+# import subprocess
+# import os
 import sys
 import pathlib
-from shutil import which
+# from shutil import which
 from .lib import utils
 from .compile_schema import compile_schema
 
 
-def get_protoc_executable():
-  if "PROTOC" in os.environ and os.path.exists(os.environ["PROTOC"]):
-    protoc = os.environ["PROTO"]
-  else:
-    protoc = which("protoc")
+# def get_protoc_executable():
+#   if "PROTOC" in os.environ and os.path.exists(os.environ["PROTOC"]):
+#     protoc = os.environ["PROTO"]
+#   else:
+#     protoc = which("protoc")
 
-  if not protoc:
-    raise RuntimeError('Procol buffer not found, please install it using given requirements.txt')
-  else:
-    return protoc
+#   if not protoc:
+#     raise RuntimeError('Procol buffer not found, please install it using given requirements.txt')
+#   else:
+#     return protoc
 
 
 def read_args(argv):
@@ -48,33 +48,33 @@ def main():
     networks = utils.load_networks(networks_dir)
 
     for network in networks:
-        proto_dir_network = output_dir / "dev"
-        output_dir_network = output_dir / "gen" / network.name
+        proto_dir_network = output_dir # / "dev"
+        # output_dir_network = output_dir / "gen" / network.name
         schema_path = naked_dir / network.name / "schema.json"
 
         compile_schema(schema_path, proto_dir_network)
-        compile_proto_files(proto_dir_network, output_dir_network, network.name)
+        # compile_proto_files(proto_dir_network, output_dir_network, network.name)
 
         print("")
 
 
 
-def compile_proto_files(proto_dir_network, output_dir_network, network_name):
-  python_dir_network = output_dir_network / "python"
-  utils.create_subtree(python_dir_network)
+# def compile_proto_files(proto_dir_network, output_dir_network, network_name):
+#   python_dir_network = output_dir_network / "python"
+#   utils.create_subtree(python_dir_network)
   
-  cpp_dir_network = output_dir_network / "cpp"
-  utils.create_subtree(cpp_dir_network)
+#   cpp_dir_network = output_dir_network / "cpp"
+#   utils.create_subtree(cpp_dir_network)
   
-  if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir_network) ,"--python_out", str(python_dir_network), str(f"{network_name}.proto")]) != 0:
-    raise RuntimeError("Proto compilation failed for Python")
-  else:
-    print(f"Generated {network_name}_pb2.py for Python into {python_dir_network}")
+#   if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir_network) ,"--python_out", str(python_dir_network), str(f"{network_name}.proto")]) != 0:
+#     raise RuntimeError("Proto compilation failed for Python")
+#   else:
+#     print(f"Generated {network_name}_pb2.py for Python into {python_dir_network}")
   
-  if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir_network) ,"--cpp_out", str(cpp_dir_network), str(f"{network_name}.proto")]) != 0:
-    raise RuntimeError("Proto compilation failed for C++")
-  else:
-    print(f"Generated {network_name}.pb.h and {network_name}.pb.cc for C++ into {cpp_dir_network}")
+#   if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir_network) ,"--cpp_out", str(cpp_dir_network), str(f"{network_name}.proto")]) != 0:
+#     raise RuntimeError("Proto compilation failed for C++")
+#   else:
+#     print(f"Generated {network_name}.pb.h and {network_name}.pb.cc for C++ into {cpp_dir_network}")
 
 if __name__ == "__main__":
   main()
