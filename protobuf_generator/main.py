8759cb2e3526a64e5024dff562a37995e5eb587f
Mattia2700 <mattia2700franzin@gmail.com>
2022-05-13 23:27:32 +0200

    chore(protobuf_generator): add timestamp to each message + temporarily remove utils

--- protobuf_generator/main.py
+++ protobuf_generator/main.py
@@ -10,25 +10,21 @@ from .generators.utils_gen.utils_gen import generate_utils
 
 
 def get_protoc_executable():
-    if "PROTOC" in os.environ and os.path.exists(os.environ["PROTOC"]):
-        protoc = os.environ["PROTO"]
-    else:
-        protoc = which("protoc")
+  if "PROTOC" in os.environ and os.path.exists(os.environ["PROTOC"]):
+    protoc = os.environ["PROTO"]
+  else:
+    protoc = which("protoc")
 
-    if not protoc:
-        raise RuntimeError(
-            "Procol buffer not found, please install it using given requirements.txt"
-        )
-    else:
-        return protoc
+  if not protoc:
+    raise RuntimeError('Procol buffer not found, please install it using given requirements.txt')
+  else:
+    return protoc
 
 
 def read_args(argv):
     # TODO: standardize
     if len(argv) != 4 or argv[1] in ["--help", "-h"]:
-        raise ValueError(
-            "Usage: python3 main.py <networks_path> <id_path> <output_path>"
-        )
+        raise ValueError("Usage: python3 main.py <networks_path> <id_path> <output_path>")
 
     networks_dir = pathlib.Path(argv[1])
     id_dir = pathlib.Path(argv[2])
@@ -61,54 +57,29 @@ def main():
         schema = compile_schema(network, proto_dir)
         compile_proto_files(proto_dir, output_dir_network, network.name)
 
-        generate_utils(schema, network, network.name, utils_dir_network)
+        # generate_utils(schema, network, network.name, utils_dir_network)
 
         print("")
 
 
+
 def compile_proto_files(proto_dir, output_dir_network, network_name):
-    python_dir_network = output_dir_network / "python"
-    utils.create_subtree(python_dir_network)
-
-    cpp_dir_network = output_dir_network / "cpp"
-    utils.create_subtree(cpp_dir_network)
-
-    if (
-        subprocess.call(
-            [
-                get_protoc_executable(),
-                "--proto_path",
-                str(proto_dir),
-                "--python_out",
-                str(python_dir_network),
-                str(f"{network_name}.proto"),
-            ]
-        )
-        != 0
-    ):
-        raise RuntimeError("Proto compilation failed for Python")
-    else:
-        print(f"Generated {network_name}_pb2.py for Python into {python_dir_network}")
-
-    if (
-        subprocess.call(
-            [
-                get_protoc_executable(),
-                "--proto_path",
-                str(proto_dir),
-                "--cpp_out",
-                str(cpp_dir_network),
-                str(f"{network_name}.proto"),
-            ]
-        )
-        != 0
-    ):
-        raise RuntimeError("Proto compilation failed for C++")
-    else:
-        print(
-            f"Generated {network_name}.pb.h and {network_name}.pb.cc for C++ into {cpp_dir_network}"
-        )
+  python_dir_network = output_dir_network / "python"
+  utils.create_subtree(python_dir_network)
+  
+  cpp_dir_network = output_dir_network / "cpp"
+  utils.create_subtree(cpp_dir_network)
+  
+  if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir) ,"--python_out", str(python_dir_network), str(f"{network_name}.proto")]) != 0:
+    raise RuntimeError("Proto compilation failed for Python")
+  else:
+    print(f"Generated {network_name}_pb2.py for Python into {python_dir_network}")
+  
+  if subprocess.call([get_protoc_executable(), "--proto_path", str(proto_dir) ,"--cpp_out", str(cpp_dir_network), str(f"{network_name}.proto")]) != 0:
+    raise RuntimeError("Proto compilation failed for C++")
+  else:
+    print(f"Generated {network_name}.pb.h and {network_name}.pb.cc for C++ into {cpp_dir_network}")
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- protobuf_generator/schema.py
+++ protobuf_generator/schema.py
@@ -19,7 +19,6 @@ from .lib import utils
     }
 """
 
-
 class Schema:
     def __init__(self, schema):
         self.types = {}
@@ -27,28 +26,22 @@ class Schema:
         if schema is not None and isinstance(schema, dict):
             for type_name, type_description in schema["types"].items():
                 if type_description["type"] == "enum":
-                    self.types[type_name] = Enum(type_name, type_description["items"])
+                    self.types[type_name]=Enum(type_name, type_description["items"])
                 elif type_description["type"] == "bitset":
                     if type_description.get("items", None) is not None:
-                        self.types[type_name] = BitSet(
-                            bitset_items=type_description["items"]
-                        )
+                        self.types[type_name]=BitSet(bitset_items=type_description["items"])
                     elif type_description.get("size", None) is not None:
-                        self.types[type_name] = BitSet(
-                            bitset_size=type_description["size"]
-                        )
+                        self.types[type_name]=BitSet(bitset_size=type_description["size"])
                 else:
-                    raise Exception(
-                        f"{type_description['type'].capitalize()} type not yet supported"
-                    )
-
+                    raise Exception(f"{type_description['type'].capitalize()} type not yet supported")
+            
             for struct_name, struct_description in schema["structs"].items():
                 self.structs[struct_name] = Struct(struct_description, self.types)
 
 
 class Enum:
     def __init__(self, type_name, enum_items: list):
-        self.proto_type = type_name + "_type"
+        self.proto_type = type_name+"_type"
         self.values = []
         for index, item_name in enumerate(enum_items):
             self.values.append(EnumItem(item_name, index))
@@ -62,12 +55,8 @@ class EnumItem:
 
 class BitSet:
     def __init__(self, bitset_items: list = None, bitset_size=None):
-        self.size = (
-            math.ceil(len(bitset_items)) / 8
-            if bitset_items
-            else math.ceil(bitset_size / 8)
-        )
-        self.proto_type = "uint32" if self.size <= 4 else "uint64"
+        self.size = math.ceil(len(bitset_items))/8 if bitset_items else math.ceil(bitset_size/8)
+        self.proto_type = "uint32" if self.size<=4 else "uint64"
 
 
 class Number:
@@ -81,19 +70,11 @@ class Number:
         elif self.original_type == "float64":
             self.original_type = "double"
             self.proto_type = "double"
-        elif (
-            self.original_type == "int8"
-            or self.original_type == "int16"
-            or self.original_type == "int32"
-        ):
+        elif self.original_type == "int8" or self.original_type == "int16" or self.original_type == "int32":
             self.proto_type = "int32"
         elif self.original_type == "int64":
             self.proto_type = "int64"
-        elif (
-            self.original_type == "uint8"
-            or self.original_type == "uint16"
-            or self.original_type == "uint32"
-        ):
+        elif self.original_type == "uint8" or self.original_type == "uint16" or self.original_type == "uint32":
             self.proto_type = "uint32"
         elif self.original_type == "uint64":
             self.proto_type = "uint64"
@@ -104,19 +85,25 @@ class Number:
 class Struct:
     def __init__(self, struct_description, types) -> None:
         self.fields = []
+        timestamp_message = False
+        last_index = 0
+        if "timestamp" in struct_description:
+            timestamp_message = True
         for index, (field_name, field_type) in enumerate(struct_description.items()):
-            self.fields.append(StructField(field_name, field_type, index + 1, types))
+            self.fields.append(StructField(field_name, field_type, index+1, types))
+            last_index += 1
+        if not timestamp_message:
+            self.fields.append(StructField("timestamp", "uint32", last_index+1, types))
 
 
 class StructField:
+    
     def __init__(self, field_name, field_type, index, types):
         self.name = field_name
         self.index = index
 
         if field_type in types:
-            if isinstance(types[field_type], Enum) or isinstance(
-                types[field_type], BitSet
-            ):
+            if isinstance(types[field_type], Enum) or isinstance(types[field_type], BitSet):
                 self.type = types[field_type]
         else:
             types[field_name] = Number(field_type)
